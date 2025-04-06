d9c516eb2d8ded3809128065866d636e5c53256c
phillipduarte <phillipdcollege@gmail.com>
2025-04-06 15:40:44 -0400

    feat: add nav bar icons

--- eruvify-app/src/BottomNavigation.js
+++ eruvify-app/src/BottomNavigation.js
@@ -6,23 +6,23 @@ function BottomNavigation() {
     <nav className="bottom-nav">
       <ul className="nav-items">
         <li className="nav-item">
-          <img src="/placeholder-icon.png" alt="Home" className="nav-icon" />
+          <img src="/assets/home-icon.png" alt="Home" className="nav-icon" />
           {/* <span>Home</span> */}
         </li>
         <li className="nav-item">
-          <img src="/placeholder-icon.png" alt="Map" className="nav-icon" />
+          <img src="/assets/map-icon.png" alt="Map" className="nav-icon" />
           {/* <span>Map</span> */}
         </li>
         <li className="nav-item">
-          <img src="/placeholder-icon.png" alt="Check" className="nav-icon" />
+          <img src="/assets/check-icon.png" alt="Check" className="nav-icon" />
           {/* <span>Check</span> */}
         </li>
         <li className="nav-item">
-          <img src="/placeholder-icon.png" alt="Notifications" className="nav-icon" />
-          {/* <span>Notifications</span> */}
+          <img src="/assets/messages-icon.png" alt="Messages" className="nav-icon" />
+          {/* <span>Messages</span> */}
         </li>
         <li className="nav-item">
-          <img src="/placeholder-icon.png" alt="Profile" className="nav-icon" />
+          <img src="/assets/profile-icon.png" alt="Profile" className="nav-icon" />
           {/* <span>Profile</span> */}
         </li>
       </ul>
